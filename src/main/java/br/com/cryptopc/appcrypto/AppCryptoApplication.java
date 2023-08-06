a4f0ff5a8e45e25fb4a3427464a9fbb47f164c04
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2023-08-05 22:53:14 -0300

    api Funcional para CORS

--- src/main/java/br/com/cryptopc/appcrypto/AppCryptoApplication.java
+++ src/main/java/br/com/cryptopc/appcrypto/AppCryptoApplication.java
@@ -2,7 +2,6 @@ package br.com.cryptopc.appcrypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class AppCryptoApplication {
