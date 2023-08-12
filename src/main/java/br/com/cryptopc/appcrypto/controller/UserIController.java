4dac65e896bfdd54aad1f71a442ee8d0287c5337
Pedro Henrique Cunha Moda <phcunha47@gmail.com>
2023-08-12 15:11:55 -0300

    updatePassword 80%
    Co-authored-by: iTrue <carlosyamanaka@users.noreply.github.com>

--- src/main/java/br/com/cryptopc/appcrypto/controller/UserIController.java
+++ src/main/java/br/com/cryptopc/appcrypto/controller/UserIController.java
@@ -116,7 +116,7 @@ public class UserIController {
                 userIRepository.save(userI);
                 return ResponseEntity.ok("Coin Added");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error");
+            return ResponseEntity.status(403).body("Error");
         }
     }
 }
